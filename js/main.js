567b825e30a4184f09705223a7e3fd155c9f6e78
Jacob Holden <yacoob.holden@gmail.com>
2016-09-22 12:50:09 +1200

    Add error feedback

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-	/* Set object to store results */
+	// Object to store results
 	var result = {};
 
+	// Handle form events
 	var form = $('#form');
-
 	form.find('.form-row').each(function(){
 		var row = $(this),
 			field = row.data('field'),
 			cols = row.find('.form-col'),
 			allowMulti = row.hasClass('allow-multi');
+		
+		// Set value in object so we can validate later
+		result[field] = {};
 	
 		cols.each(function(){
 			var col = $(this),
@@ -20,11 +23,7 @@ $(document).ready(function() {
 					// Handle multi-options
 					if (allowMulti){
 						col.addClass("selected");
-					
-						// Use obj to store multis
-						if (!result[field]){
-							result[field] = {}
-						}
+
 						result[field][value] = true;
 					} else {
 						cols.removeClass("selected");
@@ -43,7 +42,33 @@ $(document).ready(function() {
 						result[field] = undefined;
 					}
 				}
+				// Remove error marking
+				row.removeClass("error-row");
 			});
 		});
+	});
+	
+	// Handle done
+	$("#done").on("click",function(){
+		// Validate results
+		var error = false,
+			keys = Object.keys(result);
+		
+		for (var index in keys){
+			var key = keys[index];
+			if ($.isEmptyObject(result[key])){
+				var errorRow = form.find('.form-row[data-field="' + key + '"]');
+				errorRow.addClass("error-row");
+				error = true;
+			}
+		}
+		
+		// Do not continue if we have an error
+		if (error){
+			return;
+		}
+		
+		// Pass result to API
+		console.log(JSON.stringify(result));
 	});
 });
